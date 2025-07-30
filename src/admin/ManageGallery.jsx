4ed282300f17b8f8eea4eaf37ae3d107647e6fc8
abhishek <devmagnets.com>
2025-07-30 11:11:17 +0530

    cors

--- src/admin/ManageGallery.jsx
+++ src/admin/ManageGallery.jsx
@@ -17,7 +17,7 @@ export default function ManageGallery() {
 
   useEffect(() => {
     const fetchgalleryData = async () => {
-      try {
+      try {               
         const res = await fetch(`${import.meta.env.VITE_BACKEND_LINK}/manage-gallery/fetch`)
         if (res.ok) {
           const data = await res.json()
